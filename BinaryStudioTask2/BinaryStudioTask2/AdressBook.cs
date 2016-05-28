e00a7ccefc35f3a7834dab33cb99eec713472ae8
Vladyslav Hryhorenko <grigorenkovlad1993@gmail.com>
2016-05-28 23:31:52 +0300

    task2

---
+++ BinaryStudioTask2/BinaryStudioTask2/AdressBook.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+class User
+{
+    public User(string LastName, string FirstName, DateTime BirthDate, string City,
+        string Adress, string PhoneNumber, string Gender, string Email)
+    {
+        this.LastName = LastName;
+        this.FirstName = FirstName;
+        this.BirthDate = BirthDate;
+        this.City = City;
+        this.Adress = Adress;
+        this.PhoneNumber = PhoneNumber;
+        this.Gender = Gender;
+        this.Email = Email;
+    }
+
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public DateTime TimeAdded { get; set; }
+    public string City { get; set; }
+    public string Adress { get; set; }
+    public string Gender { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+
+    public override string ToString()
+    {
+        return ToString("S");
+    }
+
+    public string ToString(string fmt)
+    {
+        if (String.IsNullOrEmpty(fmt))
+            fmt = "S";
+        switch (fmt.ToUpperInvariant())
+        {
+            case "S":
+                return string.Format("[{0}, {1}, {2}, {3}]", LastName, FirstName, PhoneNumber, Adress);
+            case "F":
+                string str = "";
+                PropertyInfo[] infos = this.GetType().GetProperties();
+                foreach (PropertyInfo pi in infos)
+                {
+                    str += pi.Name + " " + pi.GetValue(this, null).ToString() + "\n";
+                }
+                return str;
+            default:
+                String msg = String.Format("'{0}' is an invalid format string", fmt);
+                throw new ArgumentException(msg);
+        }
+    }
+}
+
+class AdressBook
+{
+    private List<User> users;
+    public AdressBook()
+    {
+        users = new List<User>();
+    }
+
+    private bool Contain(User user)
+    {
+        foreach (var item in users)
+            if (item.FirstName == user.FirstName && item.LastName == user.LastName && item.Adress == user.Adress)
+                return true;
+        return false;
+    }
+
+    private User Find(User user)
+    {
+        foreach (var item in users)
+            if (item.FirstName == user.FirstName && item.LastName == user.LastName && item.Adress == user.Adress)
+                return item;
+        return null;
+    }
+
+    public void AddUser(User user)
+    {
+        try
+        {
+            user.TimeAdded = DateTime.Now;
+            if (Contain(user))
+                throw new Exception("Adress book already contains this user");
+            users.Add(user);
+            UserAdded("User " + user.ToString() + " was added");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+    }
+
+    public void RemoveUser(User user)
+    {
+        try
+        {
+            if (!Contain(user))
+                throw new Exception("Adress book doesn't contain this user");
+            users.Remove(Find(user));
+            UserRemoved("User " + user.ToString() + " was removed");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+    }
+
+    public void PrintAdressBook()
+    {
+        foreach (var i in users)
+            Console.WriteLine(i);
+    }
+
+    public delegate void SomeActionWithUser(string message);
+
+    public event SomeActionWithUser UserAdded;
+    public event SomeActionWithUser UserRemoved;
+}
+
---
+++ BinaryStudioTask2/BinaryStudioTask2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AdressBook book = new AdressBook();
+        LoggerProviderFactory factory = LoggerProviderFactory.GetInstance();
+        try
+        {
+            ILogger log = factory.GetLoggingProvider(LoggingProviders.Consol);
+            book.UserAdded += log.Info;     //подписка на события
+            book.UserRemoved += log.Info;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: "+e.Message);
+        }
+        User u1 = new User("Ivan", "Ivanov", new DateTime(1993, 4, 5), "Kyiv", "Sechenova,6", "0446785676","mail","ivan93@ukr.net");
+        User u2 = new User("Petro", "Petrenko", new DateTime(1995, 6, 7), "Lviv", "Franko,12", "0678945632", "mail","petro95@gmail.com");
+        book.AddUser(u1);
+        book.AddUser(u2);
+        book.AddUser(u1);
+        book.RemoveUser(u1);
+        book.RemoveUser(u1);
+    }
+}
